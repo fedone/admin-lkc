dddb31d09ca065326b53916c1593d917c32e9977
fedone <fedo.corazza@gmail.com>
2017-07-18 00:43:38 -0700

    event component integration with realtime db

--- src/app/anagrafica/anagrafica.component.ts
+++ src/app/anagrafica/anagrafica.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import { Subject } from 'rxjs/Subject';
 import * as firebase from 'firebase/app';
 
 
@@ -7,32 +8,36 @@ import * as firebase from 'firebase/app';
   selector: 'app-anagrafica',
   template: `
 	<ul class="demo-list-three mdl-list">
-          <li *ngFor="let item of items | async" class="mdl-list__item mdl-list__item--three-line">
-            <span class="mdl-list__item-primary-content">
-              <i class="material-icons mdl-list__item-avatar">person</i>
-              <span>{{ items.nome }}</span>
-              <span class="mdl-list__item-text-body">
-                {{ items.grado }}
-              </span>
-            </span>
-          </li>
-      </ul>
+      <li *ngFor="let item of items | async" class="mdl-list__item mdl-list__item--three-line">
+        <span class="mdl-list__item-primary-content">
+          <i class="material-icons mdl-list__item-avatar">person</i>
+          <span>{{ (item.text) }}</span>
+          <span class="mdl-list__item-text-body">
+            {{ (item.grade) }}
+          </span>
+        </span>
+      </li>
+  	</ul>
 	
   `,
   styleUrls: ['./anagrafica.component.css'],
 })
 export class AnagraficaComponent implements OnInit {
+   
+ items: FirebaseListObservable<any>;
+ sizeSubject: Subject<any>;
   
- 
- items: FirebaseListObservable<any[]>;
- nome: string;
- grado: string
-  
-  constructor(db: AngularFireDatabase ) { 
-  	this.items = db.list('/soci');
+  constructor(db: AngularFireDatabase ) {
+  	this.sizeSubject = new Subject();
+  	this.items = db.list('/soci', {
+  		query: {
+  			limitToLast: 50
+  		}
+  	});
   }
 
   ngOnInit() {
   }
+
 
 }
--- src/app/eventi/eventi.component.ts
+++ src/app/eventi/eventi.component.ts
@@ -1,17 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { AgmCoreModule } from '@agm/core';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-eventi',
-  templateUrl: './eventi.component.html',
+  template: `<div class="mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-grid">
+				<div *ngFor="let item of items | async" class="mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-grid">
+			   		<div class="mdl-card mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-cell--4-col-tablet mdl-cell--4-col-desktop">
+			      		<div class="mdl-card__title mdl-card--expand">
+			        		<img src="https://maps.googleapis.com/maps/api/staticmap?center={{ (item.lat) }},{{ (item.long) }}&zoom=5&size=400x400&maptype=roadmap&key=AIzaSyBgIfTcWVPgWam3gxncC8BTjjrrbr2nwCA" alt="">
+			     		</div>
+			      	<div class="mdl-card__supporting-text mdl-color-text--grey-600">
+			        	<h4>Evento</h4>
+			      	</div>
+			      	<ul class="list-item mdl-list">
+			        	<li class="mdl-list__item">
+			          	<span class="mdl-list__item-primary-content">
+			            	Luogo: xxxxx
+			          	</span>
+			        	</li>
+			        	<li class="mdl-list__item">
+			          	<span class="mdl-list__item-primary-content">
+			            	Data: xxxxx
+			          	</span>
+			        	</li>
+			      	</ul>
+		    	</div>
+	    	</div>
+  `,
   styleUrls: ['./eventi.component.css']
 })
 export class EventiComponent implements OnInit {
 
-  lat: number = 51.678418;
-  lng: number = 7.809007;
-
-  constructor() {}
+  items: FirebaseListObservable<any>;
+ sizeSubject: Subject<any>;
+  
+  constructor(db: AngularFireDatabase ) {
+  	this.sizeSubject = new Subject();
+  	this.items = db.list('/maps', {
+  		query: {
+  			limitToLast: 50
+  		}
+  	});
+  }
 
   ngOnInit() {
   }
