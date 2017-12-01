196cff30cacd2f5cdbc70810abd02ad91fe3c6e1
fedone <fedo.corazza@gmail.com>
2017-12-01 18:07:16 +0100

    refactoring

---
+++ sw-precache-config.js
@@ -0,0 +1,11 @@
+module.export = {
+	navigateFallback: '/index.html',
+	stripPrefix: 'dist/',
+	root: 'dist/',
+	staticFileGlobs: [
+	'/dist/index.html',
+	'/dist/**.js',
+	'/dist/**.css',
+	'/dist/assets/images/**'
+	]
+};
